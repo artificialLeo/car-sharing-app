f46c06b09bd9a1e8be4ac656659ecb60729efb6a
artificialLeo <horkovenko.k@gmail.com>
2024-02-08 12:50:55 +0200

    add test containers

--- src/main/java/com/app/car/CarApplication.java
+++ src/main/java/com/app/car/CarApplication.java
@@ -2,9 +2,12 @@ package com.app.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableScheduling
 public class CarApplication {
     public static void main(String[] args) {
--- src/test/java/com/app/car/CarApplicationTests.java
+++ src/test/java/com/app/car/CarApplicationTests.java
@@ -1,13 +1,29 @@
 package com.app.car;
 
+import com.app.car.model.User;
+import com.app.car.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Container;
 
-//@SpringBootTest
-//class CarApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CarApplicationTests {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Container
+	CustomMySqlContainer customMySqlContainer;
+
+	@Test
+	void contextLoads() {
+		customMySqlContainer.start();
+
+		userRepository.save(new User());
+	}
+
+}
