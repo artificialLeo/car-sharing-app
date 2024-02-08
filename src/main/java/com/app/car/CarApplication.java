90103e40aa28c0f38f2f60f6c3b6b7b11b41561e
artificialLeo <horkovenko.k@gmail.com>
2024-02-08 13:33:02 +0200

    add test containers props

--- src/main/java/com/app/car/CarApplication.java
+++ src/main/java/com/app/car/CarApplication.java
@@ -2,12 +2,9 @@ package com.app.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableScheduling
 public class CarApplication {
     public static void main(String[] args) {
