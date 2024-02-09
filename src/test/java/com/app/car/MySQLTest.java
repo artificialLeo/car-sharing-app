6281d65c37aa1688b559a6ad3a9d56515f6c998c
artificialLeo <horkovenko.k@gmail.com>
2024-02-09 21:20:53 +0200

    add test containers props

--- src/test/java/com/app/car/MySQLTest.java
+++ src/test/java/com/app/car/MySQLTest.java
@@ -1,28 +1,28 @@
-package com.app.car;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
-
-@SpringBootTest
-@Testcontainers
-public class MySQLTest {
-
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8");
-
-    @DynamicPropertySource
-    static void registerMySQLProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-    }
-
-
-    @Test
-    public void test() {
-        mysql.start();
-    }
-}
+//package com.app.car;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.DynamicPropertyRegistry;
+//import org.springframework.test.context.DynamicPropertySource;
+//import org.testcontainers.containers.MySQLContainer;
+//import org.testcontainers.junit.jupiter.Testcontainers;
+//
+//@SpringBootTest
+//@Testcontainers
+//public class MySQLTest {
+//
+//    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8");
+//
+//    @DynamicPropertySource
+//    static void registerMySQLProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+//        registry.add("spring.datasource.username", mysql::getUsername);
+//        registry.add("spring.datasource.password", mysql::getPassword);
+//    }
+//
+//
+//    @Test
+//    public void test() {
+//        mysql.start();
+//    }
+//}
